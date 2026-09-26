b44b086aeffc28e0adb61c7564b350f67562404e
Rohit533-max <rohit03383@gmail.com>
2026-09-26 13:42:23 +0530

    Bubble sort

--- codes/sorting.py
+++ codes/sorting.py
@@ -1,9 +1,40 @@
-def bubble_sort(arr):
-    for i in range(len(arr)):
-        for j in range(0,len(arr)-1-i):
-            if arr[j] > arr[j+1]:
-                arr[j],arr[j+1]= arr[j+1],arr[j]
+import time
+TYPE = 0
+cmp = 0
 
-    return arr
-nums = [10,5,6,2,7,14,16]
-print(bubble_sort(nums))
+def algochooser(numbers,paint, lable_comparison,something,TYPE_OF_DRAW,speed):
+    global cmp, TYPE
+    TYPE = TYPE_OF_DRAW
+    #if something == "bubble_sort":
+    lable_comparison.configure(text = "Number of comparisons")
+    bubble_sort(numbers,paint,lable_comparison,speed)
+    if TYPE == 0:
+        paint(["lawn green"] * len(numbers))
+
+    cmp = 0
+
+
+
+def bubble_sort(numbers,paint,label_comparison,speed):
+    global cmp, TYPE
+    colors = []
+    for i in range(len(numbers)):
+        is_swapped = False
+        for j in range(0,len(numbers)-1-i):
+            if numbers[j] > numbers[j+1]:
+                is_swapped = True
+                numbers[j],numbers[j+1]= numbers[j+1],numbers[j]
+
+                time.sleep(1/speed)
+            cmp +=1
+            if TYPE == 0:
+                colors = ["#cc0000" if x == numbers[j] or x == numbers[j+1] else "antique white" for x in numbers]
+
+            paint(colors)
+            label_comparison.configure(text="No. of comparisons: " + str(cmp))
+
+        #nothing swapped list is already sorted
+        if not is_swapped:
+            break
+        time.sleep(1/speed)
+
--- codes/start_sorting.py
+++ codes/start_sorting.py
@@ -1,5 +1,8 @@
 from tkinter import *
 import pygame
+from threading import *
+from sorting import algochooser
+from random import sample,shuffle
 
 class Sorting:
     def __init__(self,root):
@@ -41,9 +44,70 @@ class Sorting:
         # Algorithm Information Table
         self.information = {'Bubble Sort': "Worst Case:O(n²)\nAverage Case:O(n²)\nBest Case:O(n)"}
 
+        #algorithm names
+        self.algorithms = ['Bubble sort']
+
         #canvas of the graph
         self.Frame2 = Frame(root,width=self.canvasX, height=self.canvasY,bg="purple")
         self.Frame2.pack(side="left")
+        self.canva = Canvas(self.Frame2,width=self.canvasX,height=self.canvasY, bg="blue")
+        self.canva.pack(side='left')
+
+        #creating a drop down menu for algorithm selection
+        self.algo_var = "bubble_sort"
+
+        #label for showing the comparisons
+        self.label_comparison = Label(self.Frame1,text="Number of comparison : 0", bg="light salmon", fg="yellow", font=("Arial",10))
+        self.label_comparison.place(x = 1, y = 1)
+
+        #creating a new array
+        self.numbers = sample(range(20, self.canvasY-20),self.size.get())
+        shuffle(self.numbers)
+        self.rec_width = self.canvasX // self.size.get()
+
+        for num in self.numbers:
+            self.canva.create_rectangle(self.starting_point,self.canvasY - num, self.starting_point + self. rec_width,self.canvasY,fill = 'sandy brown')
+            self.starting_point += self.rec_width
+
+        #creating a sort button
+        self.sort_button = Button(self.Frame1, text="Sort", command= self.sort_list, bg="royal blue", fg="white", padx=2, pady=2)
+        self.sort_button.place(x=20,y=22)
+
+    def paint(self,colortype):
+        #delete the previous charts
+        self.canva.delete("all")
+
+        self.starting_point = 2
+
+        #width of each bar
+        self.rec_width = self.canvasX / self.size.get()
+
+        #bar graph implementation
+
+        for i in range(len(self.numbers)):
+            self.canva.create_rectangle(
+                self.starting_point,self.canvasY - self.numbers[i], self.starting_point + self.rec_width, self.canvasY, fill=colortype[i]
+            )
+            self.starting_point += self.rec_width
+        #update the graph frame
+        self.Frame2.update()
+
+    def new_list(self):
+        numbers = []
+        self.label_comparison.configure(text="No. of comparisons: 0")
+
+        #enter random numbers into new array
+        self.numbers = sample(range(20,self.canvasY-20), self.size.get())
+
+        if self.TYPE == 0:
+            colortype = ['sandy brown'] * len(self.numbers)
+        self.paint(colortype)
+
+    def sort_list(self):
+        self.label_comparison.configure(text = "Number of Comparisons : 0")
+        startsort = Thread(target=algochooser, args=(self.numbers,self.paint,self.label_comparison,self.algo_var,self.TYPE,self.speed.get()))
+        startsort.start()
+
 
 root = Tk()
 s = Sorting(root)
